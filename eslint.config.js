98018c7c71a2a33c280bc902a941ddbf76a93149
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-01-26 00:20:05 +0700

    apply auto formatter from prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import prettierConfig from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
-      prettierConfig,
+      prettierConfig
     ],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname
       }
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect"
       }
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'react': react,
-      'prettier': prettierPlugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react: react,
+      prettier: prettierPlugin
     },
     rules: {
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
       ],
-      'prettier/prettier': 'error',
-    },
-  },
-)
+      "prettier/prettier": "error"
+    }
+  }
+);
