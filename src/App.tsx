acecfb3f6a3e9bb57a013a525cc5ba92f7c60619
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-01-28 01:34:48 +0700

    add query params routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useSearchParams } from "react-router";
 import NameForm from "./components/name-form";
+import Envelope from "./components/envelope";
 
 function App() {
-  return (
-    <>
-      {/* <Envelope /> */}
-      <NameForm />
-    </>
-  );
+  const [params] = useSearchParams();
+
+  const name = params.get("name")?.trim() ?? "";
+
+  if (name.length > 0) {
+    return <Envelope />;
+  }
+
+  return <NameForm />;
 }
 
 export default App;
--- src/components/name-form.tsx
+++ src/components/name-form.tsx
@@ -1,10 +1,24 @@
+import { useNavigate } from "react-router";
+
 export default function NameForm() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const input = event.currentTarget.angpauname as HTMLInputElement;
+    const angpauName = input.value;
+
+    void navigate(`/?name=${angpauName}`);
+  };
+
   return (
-    <form className="name-form">
+    <form className="name-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Enter your name"
         className="name-form__input"
+        name="angpauname"
       />
       <button type="submit" className="name-form__button">
         Get You Angpau
