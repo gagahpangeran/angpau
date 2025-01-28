cde2de84a38e3e285311978c68db687f2cfce4d3
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-01-28 11:54:43 +0700

    add money to rupiah function

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,5 +1,9 @@
 import { Chance } from "chance";
 
+const rupiahValues = [100, 50, 20, 10, 5, 2, 1] as const;
+
+export type RupiahValue = (typeof rupiahValues)[number];
+
 export function nameToMoney(name: string) {
   const moneySplit = [
     { min: 1, max: 500 },
@@ -13,4 +17,16 @@ export function nameToMoney(name: string) {
   const money = rng.integer(bound);
 
   return money;
+}
+
+export function moneyToRupiah(money: number): RupiahValue[] {
+  return rupiahValues
+    .map(val => {
+      const cnt = Math.floor(money / val);
+      money = money % val;
+      return Array(cnt)
+        .fill(null)
+        .map(_ => val);
+    })
+    .flat();
 }
--- test/money.test.ts
+++ test/money.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { nameToMoney } from "../src/utils/money";
+import { moneyToRupiah, nameToMoney } from "../src/utils/money";
 
 test("string 'abc' to money", () => {
   const res = nameToMoney("abc");
@@ -9,4 +9,14 @@ test("string 'abc' to money", () => {
 test("string 'budi' to money", () => {
   const res = nameToMoney("Budi");
   expect(res).toBe(443);
+});
+
+test("'443' money to rupiah", () => {
+  const res = moneyToRupiah(443);
+  expect(res).toStrictEqual([100, 100, 100, 100, 20, 20, 2, 1]);
+});
+
+test("'88' money to rupiah", () => {
+  const res = moneyToRupiah(88);
+  expect(res).toStrictEqual([50, 20, 10, 5, 2, 1]);
 });
