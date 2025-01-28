2a6bc915ef529d43025955db8caa84c1c9c8c060
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-01-29 00:07:38 +0700

    move rupiah image to img folder

--- src/components/rupiah.tsx
+++ src/components/rupiah.tsx
@@ -1,22 +1,5 @@
-import r100 from "/100.jpeg";
-import r50 from "/50.jpeg";
-import r20 from "/20.jpeg";
-import r10 from "/10.jpeg";
-import r5 from "/5.jpeg";
-import r2 from "/2.jpeg";
-import r1 from "/1.jpeg";
 import { type RupiahValue } from "../utils/money";
 import classModifiers from "../utils/css";
-
-const rupiahImg: Record<RupiahValue, string> = {
-  100: r100,
-  50: r50,
-  20: r20,
-  10: r10,
-  5: r5,
-  2: r2,
-  1: r1
-};
 
 interface RupiahProps {
   rupiahValue: RupiahValue;
@@ -26,7 +9,7 @@ export default function Rupiah({ rupiahValue }: RupiahProps) {
   return (
     <img
       className={classModifiers("rupiah", `${rupiahValue}`)}
-      src={rupiahImg[rupiahValue]}
+      src={`/img/${rupiahValue}.jpeg`}
     />
   );
 }
