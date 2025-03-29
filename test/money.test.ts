ea63d8fc1918dc255af5774071d447d1bac01b89
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-03-29 12:22:15 +0700

    add more money test for big value

--- test/money.test.ts
+++ test/money.test.ts
@@ -11,6 +11,16 @@ test("string 'budi' to money", () => {
   expect(res).toBe(89);
 });
 
+test("string 'Muhammad Ardana' to money", () => {
+  const res = nameToMoney("Muhammad Ardana");
+  expect(res).toBe(420);
+});
+
+test("string 'Audrey Brown' to money", () => {
+  const res = nameToMoney("Audrey Brown");
+  expect(res).toBe(2624);
+});
+
 test("'443' money to rupiah", () => {
   const res = moneyToRupiah(443);
   expect(res).toStrictEqual([100, 100, 100, 100, 20, 20, 2, 1]);
